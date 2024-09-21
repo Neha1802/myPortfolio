ee8cdafe20f1cffada7da1341bc465743780dc82
Neha Pandey <nehap1802@gmail.com>
2024-09-21 17:40:36 +0530

    Revert "Update navbar.component.css"
    This reverts commit efebad0c7438da2ce1a18beca1120a2ddec21c2d.

--- src/app/portfolio-website/education/education.component.ts
+++ src/app/portfolio-website/education/education.component.ts
@@ -11,5 +11,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EducationComponent {
   
-  
+  constructor(private route: ActivatedRoute){
+    this.route.fragment.subscribe((fragment) => {
+      if (fragment) {
+        setTimeout(() => {
+          const element = document.getElementById(fragment);
+          if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+          }
+        }, 0);
+      }
+    });
+  }
 }
